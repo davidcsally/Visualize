afe009c6f3cb1f13756ff6c9423af3a29de799e5
David Sally <davidcsally@gmail.com>
2017-09-29 19:10:48 -0700

    webpack configured to use clients react module

--- webpack.config.js
+++ webpack.config.js
@@ -4,24 +4,25 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'index.js',
-    libraryTarget: 'commonjs2' // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
+    libraryTarget: 'commonjs2', // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
+    library: 'react-sight'    
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         include: path.resolve(__dirname, 'src'),
-        exclude: /(node_modules|bower_components|build)/,
+        exclude: /(node_modules|build)/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env', "stage-2"]
+            presets: ['react', 'stage-2']
           }
         }
       }
     ]
   },
   externals: {
-    'react': 'commonjs react' // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
+    'react': 'react' // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
   }
 };
