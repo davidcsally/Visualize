d7d84e8b2106751059a320d86651f22208e558ff
David Sally <davidcsally@gmail.com>
2017-09-29 23:03:35 -0700

    updated webpack to check for react-dom

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ module.exports = {
     ]
   },
   externals: {
+    'react-dom': 'react-dom',
     'react': 'react' // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
   }
 };
